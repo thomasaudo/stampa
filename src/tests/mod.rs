b0373689d68b1b180494b0b3e218400cea2aac08
Thomas Audo <thomasaudo@MacBook-Air-de-Thomas.local>
2022-05-25 00:21:53 +0200

    :white_check_mark: Init test structure

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,1 +1,47 @@
+use std::net::TcpListener;
 
+use actix_web::web::Data;
+use mongodb::Database;
+
+use crate::{startup::run, AppState};
+
+pub struct TestApp {
+    pub address: String,
+    pub database: Database,
+}
+
+pub async fn spawn_app() -> TestApp {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let address = format!("http://127.0.0.1:{}", port);
+    let configuration = crate::config::Config::from_env().unwrap();
+
+    let database = configuration.connect_mongo().await.unwrap();
+    let app_state = Data::new(AppState {
+        database: database.clone(),
+    });
+
+    let server = run(listener, app_state).expect("Failed to bind address");
+
+    let _ = tokio::spawn(server);
+
+    TestApp { address, database }
+}
+
+#[tokio::test]
+async fn login_user() {
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    let mut map = std::collections::HashMap::new();
+    map.insert("username", "admin");
+    map.insert("password", "admin");
+
+    let response = client
+        .post(&format!("{}/login", &app.address))
+        .json(&map)
+        .send()
+        .await
+        .expect("Failed to execute request");
+    assert_eq!(200, response.status().as_u16());
+}
