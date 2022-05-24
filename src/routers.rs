66070b6e3a25bb2e04748eabede63444b718132c
Thomas Audo <thomasaudo@MacBook-Air-de-Thomas.local>
2022-05-24 19:54:55 +0200

    :art: Improve REST specification

--- src/routers.rs
+++ src/routers.rs
@@ -7,40 +7,53 @@ use actix_web::web::{self, ServiceConfig};
 
 pub fn project_router(cfg: &mut ServiceConfig) {
     cfg.service(
-        web::scope("/projects")
+        web::scope("/project")
+            // Create a project
             .route("", web::post().to(create_project))
+            // Get projects
             .route("", web::get().to(get_user_projects))
+            // Get specific project
+            .route("/{project_id}", web::get().to(get_project))
+            // Get specific project credentials
+            .route("/{project_id}/credentials", web::get().to(get_credentials))
+            // Get specific project available_users
             .route(
                 "/{project_id}/available_users",
                 web::get().to(available_users),
-            )
-            .route("/{project_id}", web::get().to(get_project))
-            .route("/{project_id}/credentials", web::get().to(get_credentials)),
+            ),
     );
 }
 
 pub fn invitation_router(cfg: &mut ServiceConfig) {
     cfg.service(
         web::scope("/invitation")
-            .route("/{project_id}/accept", web::post().to(accept_invitation))
-            .route("/{project_id}/deny", web::post().to(deny_invitation))
+            // Invite user
             .route("", web::post().to(invite_user))
-            .route("", web::get().to(get_invitations)),
+            // Get user invitations
+            .route("", web::get().to(get_invitations))
+            // Accept invitation to a specific project
+            .route("/{project_id}/accept", web::post().to(accept_invitation))
+            // Deny invitation to a specific project
+            .route("/{project_id}/deny", web::post().to(deny_invitation)),
     );
 }
 
 pub fn user_router(cfg: &mut ServiceConfig) {
-    cfg.service(web::scope("/user").route("/me", web::get().to(me)));
+    // Get user informations
+    cfg.service(web::scope("/user").route("", web::get().to(me)));
 }
 
 pub fn avatar_router(cfg: &mut ServiceConfig) {
-    cfg.service(web::scope("/avatars").route("", web::post().to(create_avatar)));
+    // Add an avatar
+    cfg.service(web::scope("/avatar").route("", web::post().to(create_avatar)));
 }
 
 pub fn public_router(cfg: &mut ServiceConfig) {
     cfg.service(
         web::scope("")
+            // Register user
             .route("/register", web::post().to(register))
+            // Login user
             .route("/login", web::post().to(login)),
     );
 }
